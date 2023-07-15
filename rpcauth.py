79b662d3586d1c3d322831d4ccfedb0a7ae4868b
rbroggi <ro_broggi@hotmail.com>
2023-07-15 16:58:38 +0200

    update config generation and environment variables

--- rpcauth.py
+++ rpcauth.py
@@ -2,11 +2,10 @@
 # Copyright (c) 2015-2021 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
-
 from argparse import ArgumentParser
 from getpass import getpass
 from secrets import token_hex, token_urlsafe
-from sys import stdout
+from sys import stderr
 import hmac
 
 def generate_salt(size):
@@ -24,35 +23,26 @@ def password_to_hmac(salt, password):
 def main():
     parser = ArgumentParser(description='Create login credentials for a JSON-RPC user')
     parser.add_argument('username', help='the username for authentication')
+    parser.add_argument('file', help='the env file where the variable RPCAUTH will be appended')
     args = parser.parse_args()
 
     args.password = getpass()
     pwd_confirm = ''
     pwd_confirm = getpass()
     if pwd_confirm != args.password:
-        print('password and password-confirm do not match')
+        print('password and password-confirm do not match', file=stderr)
         raise ValueError("Password and password-confirm do not match")
 
     # Create 16 byte hex salt
     salt = generate_salt(16)
     password_hmac = password_to_hmac(salt, args.password)
 
-    auth_string = f'rpcauth={args.username}:{salt}${password_hmac}'
-
-    replace_line('bitcoin/bitcoin.template.conf', 'bitcoin/bitcoin.conf', 'rpcauth=', auth_string)
+    append(args.file, f'RPCAUTH=\'{args.username}:{salt}${password_hmac}\'\n')
 
 
-def replace_line(template_file_path, destination_file_path, search_text, replacement):
-    with open(template_file_path, 'r') as file:
-        lines = file.readlines()
-
-    with open(destination_file_path, 'w') as file:
-        for line in lines:
-            if line.strip() and line and line.strip()[0] != '#':
-                if search_text in line:
-                    line = replacement + '\n'
-            file.write(line)
-
+def append(destination_file_path, append_str):
+    with open(destination_file_path, 'a') as file:
+        file.write(append_str)
 
 if __name__ == '__main__':
     main()
